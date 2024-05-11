29696e235f843b2e8e93c3101ddd034d53b4d4fe
Raphael Wegmann <raphael.wegmann81@gmail.com>
2024-05-11 19:34:44 +0200

    Added MSR reading and writing

--- kernel/arch/x86_64/cpu/cpu.c
+++ kernel/arch/x86_64/cpu/cpu.c
@@ -1,4 +1,7 @@
+#include "arch/x86_64/cpu/cpuid.h"
+#include "sys/error.h"
 #include <arch/x86_64/cpu/cpu.h>
+#include <stdint.h>
 
 void hlt() {
 	for(;;)
@@ -7,4 +10,20 @@ void hlt() {
 void hcf() {
 	__asm__("cli");
 	hlt();
+}
+
+// MSR
+void read_msr(uint32_t msr, uint32_t *lo, uint32_t *hi) {
+	if(!cpuid_check_feature(CPUID_FEAT_EDX_MSR)) {
+		error("MSR is not supported", ERRNO_FEAT_INVALID, true);
+		return;
+	}
+	asm volatile("rdmsr" : "=a"(*lo), "=d"(*hi) : "c"(msr));
+}
+void write_msr(uint32_t msr, uint32_t lo, uint32_t hi) {
+	if(!cpuid_check_feature(CPUID_FEAT_EDX_MSR)) {
+		error("MSR is not supported", ERRNO_FEAT_INVALID, true);
+		return;
+	}
+	asm volatile("wrmsr" : : "a"(lo), "d"(hi), "c"(msr));
 }
--- kernel/include/arch/x86_64/cpu/cpu.h
+++ kernel/include/arch/x86_64/cpu/cpu.h
@@ -1,7 +1,13 @@
 #ifndef __CPU_H__
 #define __CPU_H__
 
+#include <stdint.h>
+
 void hlt();
 void hcf();
+
+// MSR
+void read_msr(uint32_t msr, uint32_t *lo, uint32_t *hi);
+void write_msr(uint32_t msr, uint32_t lo, uint32_t hi);
 
 #endif	// __CPU_H__
