ef0ad6a86e66a7ee377aa203350434ca62348ba7
Kevin Alavik <kevin@alavik.se>
2024-05-11 19:36:40 +0200

    feat: Started on LAPIC

--- kernel/arch/x86_64/apic/apic.c
+++ kernel/arch/x86_64/apic/apic.c
@@ -1,6 +1,6 @@
 #include <arch/x86_64/apic/apic.h>
+#include <arch/x86_64/apic/lapic.h>
 #include <arch/x86_64/cpu/cpuid.h>
-
 #include <sys/error.h>
 #include <tools/logger.h>
 #include <tools/panic.h>
@@ -11,4 +11,6 @@ void init_apic() {
 	if(!cpuid_check_feature(CPUID_FEAT_EDX_APIC)) {
 		error("APIC not supported", ERRNO_FEAT_INVALID, true);
 	}
+
+	init_lapic();
 }
