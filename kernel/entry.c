f5d664267ca98f9e7a643b789c455ab186774071
Kevin Alavik <kevin@alavik.se>
2024-05-11 10:39:11 +0200

    feat: Added base reviision for limine

--- kernel/entry.c
+++ kernel/entry.c
@@ -1,12 +1,21 @@
 #include <sys/boot.h>
 
+#include <stdbool.h>
+
 static void hcf(void) {
 	asm("cli");
 	for(;;) {
 		asm("hlt");
 	}
 }
+
 void _start(void) {
-	// just hang
+#ifdef LEAF_LIMINE
+	if(LIMINE_BASE_REVISION_SUPPORTED == false) {
+		hcf();
+	}
+#endif
+
+	// Do nothing.
 	hcf();
 }
--- kernel/include/sys/boot.h
+++ kernel/include/sys/boot.h
@@ -3,6 +3,19 @@
 
 #ifdef LEAF_LIMINE
 #include <limine/limine.h>
+
+__attribute__((used,
+			   section(".requests"))) static volatile LIMINE_BASE_REVISION(2);
+
+__attribute__((used,
+			   section(".requests_start_"
+					   "marker"))) static volatile LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((
+	used,
+	section(
+		".requests_end_marker"))) static volatile LIMINE_REQUESTS_END_MARKER;
+
 #endif
 
 #endif	// __BOOT_H__
